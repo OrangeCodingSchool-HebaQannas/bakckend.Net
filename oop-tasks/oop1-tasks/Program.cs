cbc58ca03e2da271da90cb956a3f65cf004ed44b
Heba Qannas <halqannas04@gmail.com>
2026-04-20 23:18:12 +0300

    adding the first 2 oop tasks

---
+++ oop-tasks/oop1-tasks/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task1
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        private int age; 
+
+        public void SetAge(int a)
+        {
+            if (a > 0) 
+            {
+                age = a; 
+            }
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age {age}");
+        }
+
+        class Student : Person
+        {
+            public void Study()
+            {
+                Console.WriteLine($"{Name} is studying hard.");
+            }
+        }
+
+        sealed class FinalClass
+        {
+            public int Number { get; set; }
+        }
+
+        class Program
+        {
+            static void Main()
+            {
+                Student s1 = new Student();
+                s1.Name = "Sarah";
+                s1.SetAge(22);
+
+                s1.PrintInfo();
+                s1.Study();
+            }
+        }
+    }
+}
---
+++ oop-tasks/oop2-tasks/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Task2
+{
+    class Car
+    {
+        public string Make;
+        public int Year;
+        public string Type;
+        public double Price;
+        public string Model;
+        public string PalletNo;
+        public string Color;
+
+        public Car(string make, int year, string type, double price, string model, string palletNo, string color)
+        {
+            Make = make;
+            Year = year;
+            Type = type;
+            Price = price;
+            Model = model;
+            PalletNo = palletNo;
+            Color = color;
+        }
+
+        bool IsEngineOn = false;
+
+        public void StartEngine()
+        {
+            IsEngineOn = true;
+            Console.WriteLine($"The {Color} {Make} {Model}'s engine has started");
+        }
+
+        public void StopEnging()
+        {
+            IsEngineOn = false;
+            Console.WriteLine($"The {Color} {Make} {Model}'s engine has stopped");
+        }
+
+        public string GetCarDetailes()
+        {
+            return $"~~~~~~~~ Car Details ~~~~~~~~~\n" + $"Make: {Make}\n" +
+                   $"Model: {Model} ({Year})\n" + $"Type: {Type}\n" +
+                   $"Color: {Color}\n" + $"Pallet No: {PalletNo}\n" +
+                   $"Price: ${Price}\n" + $"Enging Running? {IsEngineOn}";
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Car myCar = new Car("Honda", 2024, "SUV", 32500.00, "CR-V", "10-98765" , "Matt Black");
+
+            Console.WriteLine(myCar.GetCarDetailes());
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~");
+
+            myCar.StartEngine();
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~");
+
+            //To see the engine state changes
+            Console.WriteLine(myCar.GetCarDetailes());
+
+
+        }
+    }
+}
